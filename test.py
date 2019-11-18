c000c88bcf45f48d8fe01d952a636195fa9def34
anascaxi <50590642+anascaxi@users.noreply.github.com>
2019-11-18 14:03:51 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,24 +1,33 @@
 import unittest
 
-def testador_de_cpf(cpf):
-    '''testa o cpf pra ve se é real
-    o cpf vem em string'''
-    cpfval = cpf.replace('-','')
-    cpf_valido = [int(cpfval)]
-    if len(cpf_valido) == 11:
-        return True
+def limpador_de_cpf(cpf):
+    '''limpa o cpf, transforma ele em int pah'''
+    cpf_top = ''
+    for i in cpf:
+        if i.isdigit():
+            cpf_top += i    
+
+
+def testa_cpf(cpf):
+    '''vai ver se o cpf e real'''
+    cpf_cool = limpador_de_cpf(cpf)
+    if cpf_cool == True:
+            if len(cpf_cool) == 11:
+                bb = int(cpf_cool)
+                if sum(bb) % 11 == 0:
+                    return True
     else:
-        print(len(cpf_valido))
         return False
-   
+
+
+
+
 
 
 class MyTest(unittest.TestCase):
     def test(self):
-        self.assertEqual(testador_de_cpf('116793229-32'), False)
-        self.assertEqual(testador_de_cpf('116794009-16'), True)
-        self.assertEqual(testador_de_cpf('11679322931'), True)
-        self.assertEqual(testador_de_cpf('11'), False)
+        self.assertEqual(testa_cpf('116793229-32'), False)
+        self.assertEqual(testa_cpf('116794009-16'), True)
         
 if __name__ == '__main__':
-    unitest.main()
+    unittest.main()
